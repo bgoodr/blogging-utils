6fb85486aa7b31009efd7460e1a98d7c0b241ae4
Brent Goodrick <bgoodr@gmail.com>
2010-03-14 12:06:46 -0700

    More throwaway nil block code showing the output from each of the expressions

--- blogger_template_scanner.el
+++ blogger_template_scanner.el
@@ -79,11 +79,67 @@
 ;; block to see the result, given that this file is still in lisp-interaction
 ;; mode (see top of the file):
 (when nil
-  ;;
+  ;; -----------------------------------------------------------------------------------------
   (mapconcat
    (lambda (elem)
      (pp-to-string elem))
    (bg-blogger-util-get-template-variable-references "template-stretch-denim-brents-color-scheme.xml")
    "\n")
-  ;;
+  ;; -----------------------------------------------------------------------------------------
+  (mapconcat
+   (lambda (elem)
+     (pp-to-string elem))
+   (bg-blogger-util-get-template-variable-references "template-stretch-denim-brents-color-scheme.xml")
+   "\n")
+  ;; -----------------------------------------------------------------------------------------
+  (insert (mapconcat
+	   (lambda (elem)
+	     (pp-to-string elem))
+	   (bg-blogger-util-get-template-variable-references "template-Son_of_Moto_Mean_Green_Blogging_Machine_variation.xml")
+	   "\n"))
+  ;; -----------------------------------------------------------------------------------------
+  (insert (pp (bg-blogger-util-get-template-variable-references "template-stretch-denim-brents-color-scheme.xml")))
+  (("bgColor"
+    ("body"))
+   ("borderColor"
+    ("#content-wrapper" ".post" ".profile-img" "img," "table.tr-caption-container"))
+   ("dateHeaderColor"
+    ("#comments" ".date-header" "h4"))
+   ("headerBgColor"
+    ("#header"))
+   ("headerCornersColor"
+    ("#header-wrapper"))
+   ("headerTextColor"
+    ("#header" "a," "a:visited" "h1.title" "h1.title"))
+   ("linkColor"
+    ("a:link" "a:visited"))
+   ("mainBgColor"
+    ("#content-wrapper" "#main-wrapper" "#sidebar-wrapper"))
+   ("sidebarTitleBgColor"
+    (".sidebar" "h2"))
+   ("sidebarTitleTextColor"
+    (".sidebar" "h2"))
+   ("textColor"
+    ("#footer" ".post-footer" ".post-title" ".post-title" ".post-title" ".sidebar" "a," "a:visited," "body" "strong")))
+  ;; -----------------------------------------------------------------------------------------
+  (insert (pp (bg-blogger-util-get-template-variable-references "template-Son_of_Moto_Mean_Green_Blogging_Machine_variation.xml")))
+  (("blogDescriptionColor"
+    ("#header" ".description"))
+   ("dateHeaderColor"
+    ("h2.date-header"))
+   ("hoverLinkColor"
+    ("a:active" "a:hover"))
+   ("linkColor"
+    ("a:link," "a:visited"))
+   ("mainBgColor"
+    ("#outer-wrapper"))
+   ("mainTextColor"
+    ("body"))
+   ("pageHeaderColor"
+    ("#header" "a," "a:link," "a:visited" "h1" "h1" "h1" "h1"))
+   ("sidebarHeaderColor"
+    (".sidebar" "h2"))
+   ("sidebarTextColor"
+    ("#sidebar")))
+  ;; -----------------------------------------------------------------------------------------
   )
